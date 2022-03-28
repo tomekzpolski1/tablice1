a0ffe80d914c7f8107d855b972ef3b8424b58fb9
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-28 20:05:58 +0200

    Add project files.

---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ile chcesz wpisać imion? ");
+            int rozmiar = int.Parse(Console.ReadLine());
+            string[] imiona = new string[rozmiar]; //tablica string z imionami
+            for (int i = 0; i < imiona.Length; i++) //wczytywanie imion
+            {
+                Console.WriteLine("Podaj {0} imię", i+1);
+                imiona[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < imiona.Length; i++)
+            {
+                Console.WriteLine("Imię {0}",i+1);
+                Console.WriteLine(imiona[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
